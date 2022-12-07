cdf4860a6d62846cb8510859cb4fed243aad36ec
milyazkamil <milyazkamil@gmail.com>
2022-12-07 11:42:07 +0300

    Music and pictures have been changed.

--- js/data.js
+++ js/data.js
@@ -1,32 +1,32 @@
 let songs = [
     {
-        name: "song 1",
-        path: "musics/Song 1.mp3",
-        artist: "artist 1",
-        cover: "images/cover 1.png"
+        name: "I can fly",
+        path: "musics/X2Download.app - Xcho - I can fly (Official Video) (64 kbps).mp3",
+        artist: "Xcho",
+        cover: "images/cover 1.jpg"
     },
     {
-        name: "song 2",
-        path: "musics/Song 2.mp3",
-        artist: "artist 2",
-        cover: "images/cover 2.png"
+        name: "Nerdesin",
+        path: "musics/X2Download.app - Ezhel - Nerdesin (Official Audio) (64 kbps).mp3",
+        artist: "Ezhel",
+        cover: "images/cover 2.jpg"
     },
     {
-        name: "song 3",
-        path: "musics/Song 3.mp3",
-        artist: "artist 3",
-        cover: "images/cover 3.png"
+        name: "Silhouette",
+        path: "musics/Miyagi & Эндшпиль - Silhouette (Official Video).mp3",
+        artist: "Miyagi & Эндшпиль",
+        cover: "images/cover 3.jpg"
     },
     {
-        name: "song 4",
-        path: "musics/Song 4.mp3",
-        artist: "artist 4",
-        cover: "images/cover 4.png"
+        name: "Lovely",
+        path: "musics/SnapInsta.io - Billie Eilish, Khalid - lovely (64 kbps).mp3",
+        artist: "Billie Eilish, Khalid",
+        cover: "images/cover 4.jpg"
     },
     {
-        name: "song 5",
-        path: "musics/Song 5.mp3",
-        artist: "artist 5",
-        cover: "images/cover 5.png"
+        name: "Where Are You",
+        path: "musics/Otnicka - Where Are You.mp3",
+        artist: "Otnicka",
+        cover: "images/cover 5.jpg"
     },
 ]
--- js/script.js
+++ js/script.js
@@ -4,18 +4,69 @@ const music = document.querySelector("#audio");
 const seekBar = document.querySelector(".seek-bar");
 const songName = document.querySelector(".music-name");
 const artistName = document.querySelector(".artist-name");
+
 const disk = document.querySelector(".disk");
+const libraryDiv = document.querySelector(".libraryDiv");
+const addDiv = document.querySelector(".addDiv");
+const settingsDiv = document.querySelector(".settingsDiv");
+const profileDiv = document.querySelector(".profileDiv");
+
 const currentTime = document.querySelector(".current-time");
 const musicDuration = document.querySelector(".song-duration");
 const playBtn = document.querySelector(".play-btn");
 const forwardBtn = document.querySelector(".forward-btn");
 const backwardBtn = document.querySelector(".backward-btn");
 const colorChange = document.querySelector(".colorChange");
+const phone = document.querySelector(".phone");
+const day = document.querySelector(".day");
+const night = document.querySelector(".night");
+const like = document.querySelector(".like");
+const dislike = document.querySelector(".dislike");
+const footerButtons = document.querySelectorAll(".footerButtons");
+const repeat = document.querySelector(".repeat");
+const shuffle = document.querySelector(".shuffle");
 
+const fBtn1 = document.querySelector(".fBtn1");
+const fBtn2 = document.querySelector(".fBtn2");
+const fBtn3 = document.querySelector(".fBtn3");
+const fBtn4 = document.querySelector(".fBtn4");
+const fBtn5 = document.querySelector(".fBtn5");
+
+libraryDiv.style.display = "none";
+addDiv.style.display = "none";
+settingsDiv.style.display = "none";
+profileDiv.style.display = "none";
 
 document.getElementById("mode-btn").addEventListener("click", ()=> {
     colorChange.classList.toggle("dark");
+    disk.classList.toggle("red");
+    day.classList.toggle("opaklik");
+    like.classList.toggle("like-dark");
+    dislike.classList.toggle("like-dark");
+    forwardBtn.classList.toggle("like-dark");
+    backwardBtn.classList.toggle("like-dark");
+    playBtn.classList.toggle("like-dark");
+    seekBar.classList.toggle("dark-mode");
+
+    fBtn1.classList.toggle("like-dark");
+    fBtn2.classList.toggle("like-dark");
+    fBtn3.classList.toggle("like-dark");
+    fBtn4.classList.toggle("like-dark");
+    fBtn5.classList.toggle("like-dark");
+    /*
+    footerButtons[0].classList.toggle("like-dark");
+    footerButtons[1].classList.toggle("like-dark");
+    footerButtons[2].classList.toggle("like-dark");
+    footerButtons[3].classList.toggle("like-dark");
+    footerButtons[4].classList.toggle("like-dark");
+    */
+    repeat.classList.toggle("like-dark");
+    shuffle.classList.toggle("like-dark");
+ 
+    //position relative ile arkaya div koy o dive resim ekle 
+    //resimi ortalamak için githubdaki url in sonuna center kodlarını ekle
 });
+
 
 
 playBtn.addEventListener("click", () => {
@@ -45,6 +96,8 @@ const setMusic = (i) => {
 }
 setMusic(0);
 
+
+
 //formatting time in min and seconds format
 
 const formatTime = (time) => {
@@ -64,9 +117,133 @@ setInterval(() => {
     seekBar.value = music.currentTime;
     currentTime.innerHTML = formatTime(music.currentTime);
     if (Math.floor(music.currentTime) == Math.floor(seekBar.max)) {
-        forwardBtn.click();
+        if (repeat.className.includes("active")){
+            setMusic(currentMusic);
+            playMusic();
+        } else {
+            forwardBtn.click();
+        }
     }
 }, 500);
+
+repeat.addEventListener("click", () => {
+    repeat.classList.toggle("active");
+});
+
+shuffle.addEventListener("click", () => {
+    shuffle.classList.toggle("active");
+});
+
+like.addEventListener("click", () => {
+    like.classList.toggle("active");
+});
+
+dislike.addEventListener("click", () => {
+    dislike.classList.toggle("active");
+});
+
+
+fBtn1.addEventListener("click", () => {
+    fBtn1.classList.toggle("active");
+    fBtn2.classList.remove("active");
+    fBtn3.classList.remove("active");
+    fBtn4.classList.remove("active");
+    fBtn5.classList.remove("active");
+    disk.style.display = "block";
+    libraryDiv.style.display = "none";
+    addDiv.style.display = "none";
+    settingsDiv.style.display = "none";
+    profileDiv.style.display = "none";
+});
+
+fBtn2.addEventListener("click", () => {
+    fBtn1.classList.remove("active");
+    fBtn2.classList.toggle("active");
+    fBtn3.classList.remove("active");
+    fBtn4.classList.remove("active");
+    fBtn5.classList.remove("active");
+    libraryDiv.classList.toggle("degis");
+    if (libraryDiv.className.includes("degis")) {
+        disk.style.display = "none";
+        libraryDiv.style.display = "block";
+        addDiv.style.display = "none";
+        settingsDiv.style.display = "none";
+        profileDiv.style.display = "none";
+    } else {
+        disk.style.display = "block";
+        libraryDiv.style.display = "none";
+        addDiv.style.display = "none";
+        settingsDiv.style.display = "none";
+        profileDiv.style.display = "none";
+    }
+});
+
+fBtn3.addEventListener("click", () => {
+    fBtn1.classList.remove("active");
+    fBtn2.classList.remove("active");
+    fBtn3.classList.toggle("active");
+    fBtn4.classList.remove("active");
+    fBtn5.classList.remove("active");
+    addDiv.classList.toggle("degis");
+    if (addDiv.className.includes("degis")) {
+        disk.style.display = "none";
+        libraryDiv.style.display = "none";
+        addDiv.style.display = "block";
+        settingsDiv.style.display = "none";
+        profileDiv.style.display = "none";
+    } else {
+        disk.style.display = "block";
+        libraryDiv.style.display = "none";
+        addDiv.style.display = "none";
+        settingsDiv.style.display = "none";
+        profileDiv.style.display = "none";
+    }
+});
+
+fBtn4.addEventListener("click", () => {
+    fBtn1.classList.remove("active");
+    fBtn2.classList.remove("active");
+    fBtn3.classList.remove("active");
+    fBtn4.classList.toggle("active");
+    fBtn5.classList.remove("active");
+    settingsDiv.classList.toggle("degis");
+    if (settingsDiv.className.includes("degis")) {
+        disk.style.display = "none";
+        libraryDiv.style.display = "none";
+        addDiv.style.display = "none";
+        settingsDiv.style.display = "block";
+        profileDiv.style.display = "none";
+    } else {
+        disk.style.display = "block";
+        libraryDiv.style.display = "none";
+        addDiv.style.display = "none";
+        settingsDiv.style.display = "none";
+        profileDiv.style.display = "none";
+    }
+});
+
+fBtn5.addEventListener("click", () => {
+    fBtn1.classList.remove("active");
+    fBtn2.classList.remove("active");
+    fBtn3.classList.remove("active");
+    fBtn4.classList.remove("active");
+    fBtn5.classList.toggle("active");
+    profileDiv.classList.toggle("degis");
+    
+    if (profileDiv.className.includes("degis")) {
+        disk.style.display = "none";
+        libraryDiv.style.display = "none";
+        addDiv.style.display = "none";
+        settingsDiv.style.display = "none";
+        profileDiv.style.display = "block";
+    } else {
+        disk.style.display = "block";
+        libraryDiv.style.display = "none";
+        addDiv.style.display = "none";
+        settingsDiv.style.display = "none";
+        profileDiv.style.display = "none";
+    }
+});
 
 seekBar.addEventListener("change", () => {
     music.currentTime = seekBar.value;
